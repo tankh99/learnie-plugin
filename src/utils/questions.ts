0a5203f76387e14db86b74748b591a1299820026
tankh99 <khanghou99@gmail.com>
2024-07-19 20:08:34 +0800

    feat: add questions utils

---
+++ src/utils/questions.ts
@@ -0,0 +1,57 @@
+import { createNewFile, getFile, modifyFrontmatter, readFrontmatter } from "./file";
+
+const folderPath = "_Learnie_Questions"
+
+export function formatQuestionFilename(noteId: string) {
+    return `${noteId}-question`
+}
+
+export async function getQuestions(noteId: string) {
+    const vault = this.app.vault;
+    const filename = formatQuestionFilename(noteId)
+    const questionFile = await getFile(vault, folderPath, filename)
+    if (!questionFile) {
+        console.error(`Question file not found for note: ${noteId}`)
+        return null;
+    }
+
+    const questionContent = await vault.read(questionFile)
+    const frontmatter = readFrontmatter(questionContent)
+    return frontmatter["questions"]
+
+}
+
+export async function createQuestion(noteId: string, question: string, answer: string) {
+    const vault = this.app.vault;
+
+    const filename = formatQuestionFilename(noteId)
+    const createdFile = await createNewFile(vault, folderPath, filename, "")
+    if (!createdFile) {
+        console.error(`Error creating file: ${noteId}-question`)
+        return;
+    }
+
+    await modifyFrontmatter(createdFile, {
+        questions: [
+            {
+                question, answer
+            }
+        ]
+    })
+}
+
+export async function addQuestion(noteId: string, question: string, answer: string) {
+    const vault = this.app.vault;
+    const filename = formatQuestionFilename(noteId)
+    const questionFile = await getFile(vault, folderPath, filename)
+    if (!questionFile) {
+        await createQuestion(noteId, question, answer)
+        return;
+    }
+
+    const questionContent = await vault.read(questionFile)
+    const frontmatter = readFrontmatter(questionContent)
+    const questions = frontmatter["questions"] ?? []
+    questions.push({ question, answer })
+    await modifyFrontmatter(questionFile, { questions })
+}
